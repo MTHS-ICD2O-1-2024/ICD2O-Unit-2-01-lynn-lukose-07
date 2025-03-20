70438bfb657877a955d95b2660940c61435f0dea
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-03-20 17:06:14 +0000

    edited js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
+// Updated by Lynn Lukose
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Displays an alert with the message "Hello, World!"
+ */
+function myButtonClicked() {
+  document.getElementById("answer").innerHTML = "Hello, World!"
+}
